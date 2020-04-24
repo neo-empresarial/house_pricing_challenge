b2e681bf2130dbfaab330a3bd8e8942ff7ed7199
pyc <pyc@certi.org.br>
2020-04-24 18:26:36 -0300

    Add Pipfile.lock

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Improve members data science skills with a Kaggle's challenge.',
+    description='Improve members data science skills with a Kaggles challenge.',
     author='neo-empresarial',
     license='',
 )
