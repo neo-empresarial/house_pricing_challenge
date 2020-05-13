88e71694da6d28810d31b874f4f2944b6c18c260
Victor Stefenon <victorstefenon0@gmail.com>
2020-05-13 18:02:37 -0300

    Added notebook, removed chart and code folders

--- notebooks/1.0-vse-eda-rooms/teste.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-
-def line_plt(file):
-    df = pd.read_csv(file)
-    cont = df.GarageYrBlt.value_counts()
-    cont.plot.line()
-    plt.show()
