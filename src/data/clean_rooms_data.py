dfc67a8b83260fc50b9b9c8a931e55eafe984045
pyc <pyc@certi.org.br>
2020-06-03 18:02:55 -0300

    Resolve code review comments

--- src/data/clean_rooms_data.py
+++ src/data/clean_rooms_data.py
@@ -7,7 +7,6 @@ def clean_rooms(df):
     To understand what the cleanning does, please read the notebook 2.1 and 1.1.
     '''
     
-    df.drop(columns=['BsmtFinType1','BsmtFinType2'], inplace=True)
     df['BsmtFullBath'].replace([1, 2, 3], 1, inplace=True)
     df.rename(columns={'BsmtFullBath':'1 or higher'}, inplace=True)
     df['BsmtCond'].replace(['Fa', 'Po'], 'Below avarage', inplace=True)
@@ -19,8 +18,8 @@ def clean_rooms(df):
     df['BsmtFinType1'].replace(['BLQ', 'LwQ', 'Rec'], 'Others', inplace=True)
     df['BsmtFinType2'].replace(['BLQ', 'LwQ', 'Rec'], 'Others', inplace=True)
     df['GarageCond'].replace(['TA', 'Gd' 'Ex'], 'Avarage/Above Avarage', inplace=True)
-    df['GarageCond'].replace(['Po', 'Fa', 'Below Average'], inplace=True)
-    df['HalfBath'].replace([0,1], '1 or higher', inplace=True)
+    df['GarageCond'].replace(['Po', 'Fa'], 'Below Average', inplace=True)
+    df['HalfBath'].replace([1,2], '1 or higher', inplace=True)
     df['GarageType'].replace(['Detchd', 'CarPort', 'Basment', '2Types'], "Other", inplace=True)
 
     return df
