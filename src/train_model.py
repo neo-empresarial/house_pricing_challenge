00c02c449a90be5574438f91b383de9f0665fe23
Stefenon <victorstefenon0@gmail.com>
2020-07-03 17:12:48 -0300

    Update scripts

--- src/train_model.py
+++ src/train_model.py
@@ -1,18 +1,22 @@
 from clean import clean
 from feature_selection import feature_selection
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.impute import SimpleImputer
 import joblib
+import numpy as np
 import pandas as pd
 import sys
 import os
 
 
 def preprocessing_train(df):
-    return pd.get_dummies(df, drop_first=True)
+    df = pd.get_dummies(df, drop_first=True)
+    return df
 
 
 def train_model(df):
     X_train, y_train = df.drop(columns=['SalePrice']), df[['SalePrice']]
+    X_train = preprocessing_train(X_train)
     model = DecisionTreeRegressor()
     model.fit(X_train, y_train)
     return model
@@ -23,5 +27,5 @@ if __name__ == "__main__":
     df = pd.read_csv('../data/raw/train.csv', index_col=0)
     df = clean(df)
     df = feature_selection(df)
-    model = train_model(preprocessing_train(df))
+    model = train_model(df)
     joblib.dump(model, '../models/1-regression-tree.sav')
